17f9e47f37679f88fc2e3c66cf6138fe92d9b856
Giomica <127758919+gnersa@users.noreply.github.com>
2026-08-16 22:01:46 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const SITE_URL = "https://wholelegacy.com";
+const SITE_URL = "https://wholegacy.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = [
