2d1a592674f4fb5312b31b6d5030f89a09e59458
Giomica <127758919+gnersa@users.noreply.github.com>
2026-08-16 22:41:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -829,7 +829,7 @@ export default function HomePage() {
               className="wl-brand footer-brand"
             >
               <span className="wl-brand-mark">
-                <img src="/logo-header.png" alt="WHOLEGACY" className="wl-brand-img" />
+                <img src="/logo-footer.png" alt="WHOLEGACY" className="wl-brand-img" />
               </span>
 
               <span>WHOLEGACY</span>
