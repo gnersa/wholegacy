5c5250a2739789c93e735b42e42c9809e8b536fe
Giomica <127758919+gnersa@users.noreply.github.com>
2026-08-31 23:18:26 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -152,9 +152,18 @@ export default function RootLayout({
 
         {/* 2. PENEMPATAN BADGE DMCA */}
         <div style={{ textAlign: "center", padding: "20px 0" }}>
-        <a href="//www.dmca.com/Protection/Status.aspx?ID=d9e040d4-b3d6-4992-a83c-070c01540141" title="DMCA.com Protection Status" class="dmca-badge"> <img src ="https://images.dmca.com/Badges/dmca_protected_27_120.png?ID=d9e040d4-b3d6-4992-a83c-070c01540141"  alt="DMCA.com Protection Status" /></a>  <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
-      
-        </div>
+         <a
+            href="//www.dmca.com/Protection/Status.aspx?ID=d9e040d4-b3d6-4992-a83c-070c01540141"
+            title="DMCA.com Protection Status"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="dmca-badge"
+          >
+            <img
+              src="https://images.dmca.com/Badges/dmca_protected_27_120.png?ID=d9e040d4-b3d6-4992-a83c-070c01540141"
+              alt="DMCA.com Protection Status"
+            />
+          </a>
 
         {/* 3. PENEMPATAN SCRIPT DMCA */}
         <Script
